948297a0533b872b77cce238a44c1c1490edb073
Maria Luiza <malualper@hotmail.com>
2020-05-22 21:58:17 -0300

    soma2numero

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace soma2numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string   PrimeiroNumero, SegundoNumero;
+           
+            int num1, num2, soma;
+           
+            Console.Write("Digite o primeiro numero");
+            PrimeiroNumero = Console.ReadLine();
+           
+            Console.Write("Digite o segundo numero ");
+            SegundoNumero = Console.ReadLine();
+           
+            num1 = Int32.Parse (PrimeiroNumero);
+            num2 = Int32.Parse (SegundoNumero);
+            soma = num1 + num2;
+            Console.WriteLine ("\nO resultado é {0}", soma);
+}
+    }
+}
